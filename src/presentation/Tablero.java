0bfec1215b8778c7c71bdc004f24dd4cfa4b9f8b
Johann Amaya Lopez <johannpez99@gmail.com>
2022-11-18 21:27:06 -0500

    Organization tablero

--- src/presentation/Tablero.java
+++ src/presentation/Tablero.java
@@ -22,20 +22,50 @@ public class Tablero extends JFrame {
 	public Tablero() {
 		Ventana();
 		getContentPane().setLayout(new BorderLayout(0, 0));
+		panelNorth();
+		panelWest();
+		panelCenter();
+		panelEast();
+		panelSouth();
 		
-		JPanel panel = new JPanel();
-		panel.setOpaque(false);
-		getContentPane().add(panel, BorderLayout.NORTH);
-		panel.setLayout(new BorderLayout(0, 0));
+	}
+	
+    public void Ventana() {
+		
+		this.setTitle("Damas");	
+		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+		int height = pantalla.height;
+		int width = pantalla.width;
+		setSize(width, height);		
+		setLocationRelativeTo(null);		
+		setVisible(true);
+		this.setResizable(false);
+		getContentPane().setBackground(new Color(128, 255, 255));
+		
+	}
+    
+    public void panelCenter() {
+    	JPanel panelCenter = new JPanel();
+		panelCenter.setBackground(new Color(0, 128, 0));
+		getContentPane().add(panelCenter, BorderLayout.CENTER);
+    	
+    }
+    
+    public void panelNorth() {
+    	
+    	JPanel panelNorth = new JPanel();
+		panelNorth.setOpaque(false);
+		getContentPane().add(panelNorth, BorderLayout.NORTH);
+		panelNorth.setLayout(new BorderLayout(0, 0));
 		
 		JLabel lblNewLabel = new JLabel("DAMAS");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
-		panel.add(lblNewLabel);
+		panelNorth.add(lblNewLabel);
 		
 		JPanel panel_10 = new JPanel();
 		panel_10.setOpaque(false);
-		panel.add(panel_10, BorderLayout.SOUTH);
+		panelNorth.add(panel_10, BorderLayout.SOUTH);
 		panel_10.setLayout(new BorderLayout(0, 0));
 		
 		JLabel lblNewLabel_19 = new JLabel("10:00");
@@ -64,7 +94,7 @@ public class Tablero extends JFrame {
 		panel_10.add(panel_13, BorderLayout.EAST);
 		panel_13.setOpaque(false);
 		panel_13.setLayout(new BorderLayout(0, 0));
-		
+    	
 		JLabel lblNewLabel_3_1 = new JLabel("Player 2");
 		lblNewLabel_3_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_3_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -76,38 +106,20 @@ public class Tablero extends JFrame {
 		JButton btnNewButton_1_1 = new JButton(" ");
 		btnNewButton_1_1.setBackground(Color.GREEN);
 		panel_13.add(btnNewButton_1_1, BorderLayout.WEST);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setOpaque(false);
-		getContentPane().add(panel_1, BorderLayout.SOUTH);
-		
-		JLabel lblNewLabel_6 = new JLabel(" ");
-		panel_1.add(lblNewLabel_6);
-		
-		JPanel panel_2 = new JPanel();
-		panel_2.setOpaque(false);
-		getContentPane().add(panel_2, BorderLayout.EAST);
-		panel_2.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblNewLabel_10 = new JLabel("                                                    ");
-		panel_2.add(lblNewLabel_10, BorderLayout.SOUTH);
-		
-		JPanel panel_8 = new JPanel();
-		panel_2.add(panel_8, BorderLayout.NORTH);
-		panel_8.setOpaque(false);
-		panel_8.setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(new Color(128, 255, 255));
-		getContentPane().add(panel_3, BorderLayout.WEST);
-		panel_3.setLayout(new BorderLayout(0, 0));
+    }
+    
+    public void panelWest() {
+    	JPanel panelWest = new JPanel();
+		panelWest.setBackground(new Color(128, 255, 255));
+		getContentPane().add(panelWest, BorderLayout.WEST);
+		panelWest.setLayout(new BorderLayout(0, 0));
 		
 		JLabel lblNewLabel_2 = new JLabel("                                                    ");
-		panel_3.add(lblNewLabel_2, BorderLayout.CENTER);
+		panelWest.add(lblNewLabel_2, BorderLayout.CENTER);
 		
 		JPanel panel_5 = new JPanel();
 		panel_5.setOpaque(false);
-		panel_3.add(panel_5, BorderLayout.SOUTH);
+		panelWest.add(panel_5, BorderLayout.SOUTH);
 		
 		JButton btnExit = new JButton("Exit");
 		btnExit.addActionListener(new ActionListener() {
@@ -119,29 +131,29 @@ public class Tablero extends JFrame {
 		});
 		btnExit.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		panel_5.add(btnExit);
-		
-		JPanel panel_4 = new JPanel();
-		panel_4.setBackground(new Color(0, 128, 0));
-		getContentPane().add(panel_4, BorderLayout.CENTER);
-	}
-	
-    public void Ventana() {
-		
-		this.setTitle("Damas");	
-		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
-		int height = pantalla.height;
-		int width = pantalla.width;
-		setSize(width, height);		
-		setLocationRelativeTo(null);		
-		setVisible(true);
-		this.setResizable(false);
-		getContentPane().setBackground(new Color(128, 255, 255));
-		
-	}
-    
-    public void PanelCenter_Tablero() {
-    	JPanel panel = new JPanel();
-		getContentPane().add(panel, BorderLayout.CENTER);
     }
-
+    
+    public void panelEast() {
+    	JPanel panelEast = new JPanel();
+		panelEast.setOpaque(false);
+		getContentPane().add(panelEast, BorderLayout.EAST);
+		panelEast.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel_10 = new JLabel("                                                    ");
+		panelEast.add(lblNewLabel_10, BorderLayout.SOUTH);
+		
+		JPanel panel_8 = new JPanel();
+		panelEast.add(panel_8, BorderLayout.NORTH);
+		panel_8.setOpaque(false);
+		panel_8.setLayout(new BorderLayout(0, 0));
+    }
+    
+    public void panelSouth() {
+    	JPanel panelSouth = new JPanel();
+		panelSouth.setOpaque(false);
+		getContentPane().add(panelSouth, BorderLayout.SOUTH);
+		
+		JLabel lblNewLabel_6 = new JLabel(" ");
+		panelSouth.add(lblNewLabel_6);
+    }
 }
