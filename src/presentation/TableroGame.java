f87f72462bf4469f5a6cbca70235a2b59fcacfaa
unknown <johann.amaya@labinfo.is.escuelaing.edu.co>
2022-11-29 18:16:38 -0500

    botones clic

--- src/presentation/TableroGame.java
+++ src/presentation/TableroGame.java
@@ -7,6 +7,7 @@ import java.awt.event.ComponentListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class TableroGame extends JPanel implements ComponentListener {
@@ -17,7 +18,11 @@ public class TableroGame extends JPanel implements ComponentListener {
     private static final long serialVersionUID = 1L;
     private JButton[][] nCasillas = null;
     private int nMatriz = 10;
+    private int TableroVis[][]  = new int[nMatriz][nMatriz];
+    private int verdes[][] = null;
+    private int amari[][] = null;
     private int mSeparacion = 2;
+    
 
     public void acomodar() {
         int ancho = this.getWidth();
@@ -34,12 +39,11 @@ public class TableroGame extends JPanel implements ComponentListener {
         for (int i = 0; i < nMatriz; i++) {
             for (int j = 0; j < nMatriz; j++) {
                 JButton temp = nCasillas[i][j];
-
-                temp.setBounds(xOffset + j * tamaAncho, yOffset + i * tamaAlto, tamaAncho - mSeparacion, tamaAlto - mSeparacion);
-
+                TableroVis[i][j]= 0;
+                temp.setBounds(xOffset + j * tamaAncho, yOffset + i * tamaAlto, tamaAncho - mSeparacion, tamaAlto - mSeparacion);             
             }
         }
-
+        
     }
 
     /**
@@ -63,12 +67,18 @@ public class TableroGame extends JPanel implements ComponentListener {
                 JButton temp = new JButton();
                 ImageIcon peon = new ImageIcon();
                 temp.setBackground(Color.WHITE);
+                
                 if ((i + j) % 2 != 0) {
                     temp.setBackground(Color.BLACK);
+                    TableroVis[i][j]= 1;
                     if ((nMatriz / 2) - 2 >= i) {
                         temp.setIcon(new ImageIcon(TableroGame.class.getResource("/image/peonV.png")));
+                        TableroVis[i][j]= 2;
+                        verdes[i][j]=2;
                     } else if ((nMatriz / 2) + 1 <= i) {
                         temp.setIcon(new ImageIcon(TableroGame.class.getResource("/image/peonA.png")));
+                        TableroVis[i][j]= 3;
+                        amari[i][j]= 3;
                     }
                 }
                 nCasillas[i][j] = temp;
@@ -76,7 +86,31 @@ public class TableroGame extends JPanel implements ComponentListener {
 
             }
         }
+        
     }
+    
+    public void juego() {
+    	for (int i = 0; i < nCasillas.length; i++) {
+    		   for (int j = 0; j < nCasillas[].length; j++) {
+    			   nCasillas[i]j].setOnClickListener(new OnClickListener (){
+    		                   public void onClick(View view){ 
+    		                         //haces lo que necesites
+    		                   }  
+    		              });   
+    		   }  
+    		}
+    }
+    
+    public void validar() {
+    	if (TableroVis[x][y] != 0) {
+    		JOptionPane.showMessageDialog(null, "Esta casilla no esta vacia","ALERTA!!!",JOptionPane.PLAIN_MESSAGE);
+    	}
+    }
+    
+    public void moverficha() {
+    	validar();
+    }
+    
 
     public void componentResized(ComponentEvent e) {
 
